b7841135a4c5c22a59a2e5268fbee12fe5466588
Suzane Luz <azevedossuzane@gmail.com>
2026-08-19 17:57:12 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-// Smooth scroll para botão "Veja meus projetos"
-document.getElementById('btn-projetos').addEventListener('click', () => {
-  document.getElementById('projetos').scrollIntoView({ behavior: 'smooth' });
-});
